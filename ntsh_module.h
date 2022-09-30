0b1a072dd3058880a579916a8c6c295cdd7d851f
ZaOniRinku <zaonirinku@gmail.com>
2022-09-30 17:56:33 +0200

    fix paths

--- ntsh_module.h
+++ ntsh_module.h
@@ -1,6 +1,5 @@
 #pragma once
-#include "ntsh_module_interface.h"
-#include "../external/Common/ntsh_module_interface.h"
+#include "external/Common/ntsh_module_interface.h"
 
 class NutshellModule : public NutshellModuleInterface {
 public:
