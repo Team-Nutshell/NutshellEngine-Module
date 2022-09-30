64ea59c8fbd3bcbda3a0946cd1aca1714e07c072
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-30 23:19:00 +0200

    default behaviour for non-implemented module functions

--- ntsh_module.h
+++ ntsh_module.h
@@ -5,7 +5,15 @@ class NutshellModule : public NutshellModuleInterface {
 public:
     NutshellModule() : NutshellModuleInterface(NTSH_MODULE_UNKNOWN, "NutshellModule") {}
 
-    virtual void init() = 0;
-    void update(double dt) = 0;
-    void destroy() = 0;
+    void init() {
+        NTSH_MODULE_WARNING("init() function not implemented.");
+    }
+
+    void update(double dt) {
+        NTSH_MODULE_WARNING("update() function not implemented.");
+    }
+
+    void destroy() {
+        NTSH_MODULE_WARNING("destroy() function not implemented.");
+    }
 };
