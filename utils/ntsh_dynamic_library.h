645ad15538db8b17e8b8dff07bdd8cc1343044c4
ZaOniRinku <zaonirinku@gmail.com>
2022-12-15 14:59:21 +0100

    don't use ifdef anymore

--- utils/ntsh_dynamic_library.h
+++ utils/ntsh_dynamic_library.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#ifdef NTSH_OS_WINDOWS
-#ifdef NutshellModule_EXPORTS
+#if defined(NTSH_OS_WINDOWS)
+#if defined(NutshellModule_EXPORTS)
 #define NTSH_MODULE_API __declspec(dllexport)
 #else
 #define NTSH_MODULE_API __declspec(dllimport)
--- utils/ntsh_module_defines.h
+++ utils/ntsh_module_defines.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 
-#ifdef NTSH_CURRENT_MODULE_GRAPHICS
+#if defined(NTSH_CURRENT_MODULE_GRAPHICS)
 	#define NTSH_MODULE_MESSAGE_PREFIX "\33[1m\33[92mGRAPHICS "
 #elif defined(NTSH_CURRENT_MODULE_PHYSICS)
 	#define NTSH_MODULE_MESSAGE_PREFIX "\33[1m\33[35mPHYSICS "
@@ -13,7 +13,7 @@
 	#define NTSH_MODULE_MESSAGE_PREFIX  "\33[1mUNKNOWN "
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_MODULE_INFO(message) \
 	do { \
 		std::cerr << NTSH_MODULE_MESSAGE_PREFIX << "\33[39mMODULE \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
@@ -24,7 +24,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_MODULE_WARNING(message) \
 	do { \
 		std::cerr << NTSH_MODULE_MESSAGE_PREFIX << "\33[39mMODULE \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
@@ -35,7 +35,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_MODULE_ERROR(message, code) \
 	do { \
 		std::cerr << NTSH_MODULE_MESSAGE_PREFIX << "\33[39mMODULE \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
@@ -48,7 +48,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_MODULE_FUNCTION_NOT_IMPLEMENTED() \
 	do { \
 		std::cerr << NTSH_MODULE_MESSAGE_PREFIX << "\33[39mMODULE\33[0m FUNCTION NOT IMPLEMENTED: " << __func__ << std::endl; \
