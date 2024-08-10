1832b274259a70424ab7c31eb7a6f897d81dcde7
ntsh-oni <teamnutshell.dev@gmail.com>
2024-08-10 20:14:29 +0200

    replace ostreams for strings to avoid unwanted flushes

--- utils/ntshengn_module_defines.h
+++ utils/ntshengn_module_defines.h
@@ -1,24 +1,25 @@
 #pragma once
 #include <iostream>
+#include <string>
 
 #if defined(NTSHENGN_CURRENT_MODULE_GRAPHICS)
-	#define NTSHENGN_MODULE_MESSAGE_PREFIX "\33[1m\33[92mGRAPHICS "
+	#define NTSHENGN_MODULE_MESSAGE_PREFIX std::string("\33[1m\33[92mGRAPHICS ")
 #elif defined(NTSHENGN_CURRENT_MODULE_PHYSICS)
-	#define NTSHENGN_MODULE_MESSAGE_PREFIX "\33[1m\33[35mPHYSICS "
+	#define NTSHENGN_MODULE_MESSAGE_PREFIX std::string("\33[1m\33[35mPHYSICS ")
 #elif defined(NTSHENGN_CURRENT_MODULE_WINDOW)
-	#define NTSHENGN_MODULE_MESSAGE_PREFIX "\33[1m\33[93mWINDOW "
+	#define NTSHENGN_MODULE_MESSAGE_PREFIX std::string("\33[1m\33[93mWINDOW ")
 #elif defined(NTSHENGN_CURRENT_MODULE_AUDIO)
-	#define NTSHENGN_MODULE_MESSAGE_PREFIX "\33[1m\33[34mAUDIO "
+	#define NTSHENGN_MODULE_MESSAGE_PREFIX std::string("\33[1m\33[34mAUDIO ")
 #elif defined(NTSHENGN_CURRENT_MODULE_ASSET_LOADER)
-	#define NTSHENGN_MODULE_MESSAGE_PREFIX "\33[1m\33[36mASSET LOADER "
+	#define NTSHENGN_MODULE_MESSAGE_PREFIX std::string("\33[1m\33[36mASSET LOADER ")
 #else
-	#define NTSHENGN_MODULE_MESSAGE_PREFIX  "\33[1mUNKNOWN "
+	#define NTSHENGN_MODULE_MESSAGE_PREFIX std::string("\33[1mUNKNOWN ")
 #endif
 
 #if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_MODULE_INFO(message) \
 	do { \
-		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX << "\33[39mMODULE \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
+		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX + "\33[39mMODULE \33[34mINFO\33[39m\33[0m: " + std::string(message) << std::endl; \
 	} while(0)
 #else
 #define NTSHENGN_MODULE_INFO(message) \
@@ -29,7 +30,7 @@
 #if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_MODULE_WARNING(message) \
 	do { \
-		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX << "\33[39mMODULE \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
+		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX + "\33[39mMODULE \33[93mWARNING\33[39m\33[0m: " + std::string(message) << std::endl; \
 	} while(0)
 #else
 #define NTSHENGN_MODULE_WARNING(message) \
@@ -40,7 +41,7 @@
 #if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_MODULE_ERROR(message, code) \
 	do { \
-		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX << "\33[39mMODULE \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
+		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX + "\33[39mMODULE \33[31mERROR\33[39m\33[0m: " + std::string(message) + " (" + std::string(#code) + ")" << std::endl; \
 		exit(1); \
 	} while(0)
 #else
@@ -53,7 +54,7 @@
 #if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED() \
 	do { \
-		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX << "\33[39mMODULE\33[0m FUNCTION NOT IMPLEMENTED: " << __func__ << std::endl; \
+		std::cerr << NTSHENGN_MODULE_MESSAGE_PREFIX + "\33[39mMODULE\33[0m FUNCTION NOT IMPLEMENTED: " + std::string(__func__) << std::endl; \
 	} while(0)
 #else
 #define NTSHENGN_MODULE_FUNCTION_NOT_IMPLEMENTED() \
